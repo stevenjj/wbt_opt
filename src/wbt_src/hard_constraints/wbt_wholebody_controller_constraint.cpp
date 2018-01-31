59ed74f6946005f2f18a7c4706b97e884f63c8a0
Steven Jens Jorgensen <stevenjensj@gmail.com>
2018-01-30 20:59:51 -0600

    implemented sparse matrix for WBC constraint

--- src/wbt_src/hard_constraints/wbt_wholebody_controller_constraint.cpp
+++ src/wbt_src/hard_constraints/wbt_wholebody_controller_constraint.cpp
@@ -241,12 +241,65 @@ void Wholebody_Controller_Constraint::evaluate_sparse_A_matrix(const int &timest
 
   std::cout << "[WBC Constraint] Evaluating A Matrix" << std::endl;
 
-  sejong::Matrix pre_zeroBlock(n, m*timestep);
-  sejong::Matrix post_zeroBlock(n, m*(T-1-timestep));  
+  sejong::Matrix pre_zeroBlock(n, m*timestep); pre_zeroBlock.setZero();
+  sejong::Matrix post_zeroBlock(n, m*(T-1-timestep));  post_zeroBlock.setZero();
+  sejong::Matrix kf_zeroBlock(n, k);    kf_zeroBlock.setZero();
 
 
-  std::cout << "Size of 0 vector: " << test_vec.size() << std::endl;
-  std::cout << "   num of keyframe vars: " << k << std::endl;
+  // Matrix A = [0, 0, ..., dWBC_i/dx, 0, ..., 0_{T-1-i}, 0] 
+  int local_j_offset = 0;
+  int i_local = 0;               
+  int j_local = local_j_offset;
+  // Add pre zero block:
+  std::cout << "[WBC Constraint] Constructing Pre Zero block with size: (" << pre_zeroBlock.rows() << "," << pre_zeroBlock.cols() << ")" << std::endl;
+  std::cout << "[WBC Constraint] Starting with j index: " << j_local << std::endl;  
+  for(size_t i = 0; i < pre_zeroBlock.rows(); i++){
+    for(size_t j = 0; j < pre_zeroBlock.cols(); j++){
+      A.push_back(pre_zeroBlock(i,j));
+      iA.push_back(i_local);
+      jA.push_back(j_local);
+      j_local++;
+    }
+    i_local++;
+    j_local = local_j_offset; // Reset counter    
+  }
+
+  // Add post zero block
+  local_j_offset = m*(timestep+1);
+  i_local = 0;
+  j_local = local_j_offset;
+  std::cout << "[WBC Constraint] Constructing Post Zero block with size: (" << post_zeroBlock.rows() << "," << post_zeroBlock.cols() << ")" << std::endl;
+  std::cout << "[WBC Constraint] Starting with j index: " << j_local << std::endl;  
+  for(size_t i = 0; i < post_zeroBlock.rows(); i++){
+    for(size_t j = 0; j < post_zeroBlock.cols(); j++){
+      A.push_back(post_zeroBlock(i,j));
+      iA.push_back(i_local);
+      jA.push_back(j_local);
+      j_local++;
+    }
+    i_local++;
+    j_local = local_j_offset; // Reset counter    
+  }
+
+  // Add zeros on the keyframe block
+  local_j_offset = m*T;
+  i_local = 0;
+  j_local = local_j_offset;
+
+  std::cout << "[WBC Constraint] Constructing Keyframe block with size: (" << kf_zeroBlock.rows() << "," << kf_zeroBlock.cols() << ")" << std::endl;
+  std::cout << "[WBC Constraint] Starting with j index: " << j_local << std::endl;  
+  for(size_t i = 0; i < kf_zeroBlock.rows(); i++){
+    for(size_t j = 0; j < kf_zeroBlock.cols(); j++){
+      A.push_back(kf_zeroBlock(i,j));
+      iA.push_back(i_local);
+      jA.push_back(j_local);
+      j_local++;
+    }
+    i_local++;
+    j_local = local_j_offset; // Reset counter    
+  }
+
+
 }
 
 
