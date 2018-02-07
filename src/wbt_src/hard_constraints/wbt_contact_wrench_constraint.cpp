bd4f2f90bc2b9fbcfecbc59d1778b8f80d559e5c
Steven Jens Jorgensen <stevenjensj@gmail.com>
2018-02-06 18:18:37 -0600

    added more notes on implementation plan and create setU from previous optimization

--- src/wbt_src/hard_constraints/wbt_contact_wrench_constraint.cpp
+++ src/wbt_src/hard_constraints/wbt_contact_wrench_constraint.cpp
@@ -63,3 +63,50 @@ void Contact_Wrench_Constraint::evaluate_sparse_A_matrix(const int &timestep, WB
 
 }
 
+void Contact_Wrench_Constraint::UpdateUf(const sejong::Vector &q_state, sejong::Matrix &Uf){
+
+
+}
+
+void Contact_Wrench_Constraint::setU(const double x, const double y, const double mu, sejong::Matrix & U){
+  U = sejong::Matrix::Zero(17, 6);
+
+  U(0, 5) = 1.;
+
+  U(1, 3) = 1.; U(1, 5) = mu;
+  U(2, 3) = -1.; U(2, 5) = mu;
+
+  U(3, 4) = 1.; U(3, 5) = mu;
+  U(4, 4) = -1.; U(4, 5) = mu;
+
+  U(5, 0) = 1.; U(5, 5) = y;
+  U(6, 0) = -1.; U(6, 5) = y;
+
+  U(7, 1) = 1.; U(7, 5) = x;
+  U(8, 1) = -1.; U(8, 5) = x;
+
+  // Tau
+  U(9, 0) = -mu; U(9, 1) = -mu; U(9, 2) = 1;
+  U(9, 3) = y;   U(9, 4) = x;   U(9, 5) = (x + y)*mu;
+
+  U(10, 0) = -mu; U(10, 1) = mu; U(10, 2) = 1;
+  U(10, 3) = y;   U(10, 4) = -x; U(10, 5) = (x + y)*mu;
+
+  U(11, 0) = mu; U(11, 1) = -mu; U(11, 2) = 1;
+  U(11, 3) = -y; U(11, 4) = x;   U(11, 5) = (x + y)*mu;
+
+  U(12, 0) = mu; U(12, 1) = mu; U(12, 2) = 1;
+  U(12, 3) = -y; U(12, 4) = -x; U(12, 5) = (x + y)*mu;
+  /////////////////////////////////////////////////
+  U(13, 0) = -mu; U(13, 1) = -mu; U(13, 2) = -1;
+  U(13, 3) = -y;  U(13, 4) = -x;  U(13, 5) = (x + y)*mu;
+
+  U(14, 0) = -mu; U(14, 1) = mu; U(14, 2) = -1;
+  U(14, 3) = -y;  U(14, 4) = x;  U(14, 5) = (x + y)*mu;
+
+  U(15, 0) = mu; U(15, 1) = -mu; U(15, 2) = -1;
+  U(15, 3) = y;  U(15, 4) = -x;  U(15, 5) = (x + y)*mu;
+
+  U(16, 0) = mu; U(16, 1) = mu; U(16, 2) = -1;
+  U(16, 3) = y;  U(16, 4) = x;  U(16, 5) = (x + y)*mu;
+}
