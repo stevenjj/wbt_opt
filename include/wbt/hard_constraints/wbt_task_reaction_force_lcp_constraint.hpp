3f2fc44e357587408765999275032c0fac229a2a
Steven Jens Jorgensen <stevenjensj@gmail.com>
2018-02-12 16:14:08 -0600

    use generic constraint size for lcp constraint. begin task and reaction force constraint

--- include/wbt/hard_constraints/wbt_task_reaction_force_lcp_constraint.hpp
+++ include/wbt/hard_constraints/wbt_task_reaction_force_lcp_constraint.hpp
@@ -1,5 +1,5 @@
-#ifndef WBT_CONTACT_LCP_CONSTRAINT_H
-#define WBT_CONTACT_LCP_CONSTRAINT_H
+#ifndef WBT_TASK_REACTION_FORCE_LCP_CONSTRAINT_H
+#define WBT_TASK_REACTION_FORCE_LCP_CONSTRAINT_H
 
 #include <Utils/wrap_eigen.hpp>
 
@@ -8,18 +8,22 @@
 
 #include <wbt/hard_constraints/wbt_constraint_main.hpp>
 #include <wbt/containers/wbt_contact_list.hpp>
+#include <wbt/containers/wbt_wholebody_task_list.hpp>
 
 #include "RobotModel.hpp"
 
-class Contact_LCP_Constraint: public Constraint_Function{
+class Task_Reaction_Force_LCP_Constraint: public Constraint_Function{
 public:
-	Contact_LCP_Constraint();
-	Contact_LCP_Constraint(Contact_List* contact_list_in, int index_in);	
-	~Contact_LCP_Constraint();
+	Task_Reaction_Force_LCP_Constraint();
+	Task_Reaction_Force_LCP_Constraint(WholeBody_Task_List* wb_task_list_in, Contact_List* contact_list_in, int index_in);		
+	~Task_Reaction_Force_LCP_Constraint();
 
 	RobotModel* robot_model;	
 
 	void setContact_List(Contact_List* contact_list_in);
+	void setTask_List(WholeBody_Task_List* wb_task_list_in);	
+
+	void setTask_index(int index_in);	
 	void setContact_index(int index_in);	
 
 	void evaluate_constraint(const int &timestep, WBT_Opt_Variable_List& var_list, std::vector<double>& F_vec);
@@ -27,8 +31,11 @@ public:
 	void evaluate_sparse_A_matrix(const int &timestep, WBT_Opt_Variable_List& var_list, std::vector<double>& A, std::vector<int>& iA, std::vector<int>& jA);
 
 private:
-	const int num_constraints = 2;
+	const int num_constraints = 1;
 	Contact_List* contact_list_obj;
+	WholeBody_Task_List* wb_task_list_obj;
+
+	int task_index = -1;	
 	int contact_index = -1;	
 
 	void Initialization();
--- src/wbt_src/hard_constraints/wbt_contact_lcp_constraint.cpp
+++ src/wbt_src/hard_constraints/wbt_contact_lcp_constraint.cpp
@@ -92,7 +92,7 @@ void Contact_LCP_Constraint::evaluate_sparse_gradient(const int &timestep, WBT_O
 }
 
 void Contact_LCP_Constraint::evaluate_sparse_A_matrix(const int &timestep, WBT_Opt_Variable_List& var_list, std::vector<double>& A, std::vector<int>& iA, std::vector<int>& jA){
-  int n = this->num_constraints;
+  int n = this->get_constraint_size();//this->num_constraints;
   int m = var_list.get_size_timedependent_vars(); // var_list.get_num_time_dependent_vars
   int T = var_list.total_timesteps; // var_list.get_total_timesteps() Total timestep
   int k = var_list.get_num_keyframe_vars();
